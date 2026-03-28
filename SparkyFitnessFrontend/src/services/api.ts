e57a6eac45645b7f93256868e8be57929b7fb815
KraLeoD <165140689+KraLeoD@users.noreply.github.com>
2026-03-28 14:30:20 +0000

    disable

--- SparkyFitnessFrontend/src/services/api.ts
+++ SparkyFitnessFrontend/src/services/api.ts
@@ -205,12 +205,10 @@ async function performRedirectToLogin() {
   }, delay);
 }
 
-// eslint-disable @typescript-eslint/no-explicit-any
 export async function apiCall(
   endpoint: string,
   options?: ApiCallOptions
-): Promise<any> {
-// eslint-enable @typescript-eslint/no-explicit-any
+): Promise<any> { // eslint-disable-line @typescript-eslint/no-explicit-any
   const userLoggingLevel = getUserLoggingLevel();
   let url = options?.externalApi ? endpoint : `${API_BASE_URL}${endpoint}`;
 
